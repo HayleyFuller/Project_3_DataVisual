5721e62feaa2605dc032720e9d3f4c1f4f456401
HayleyFuller <122294137+HayleyFuller@users.noreply.github.com>
2023-05-18 21:59:39 +0100

    Some changes to the code

--- app.js
+++ app.js
@@ -1,26 +1,40 @@
 
-const url = "https://data.wa.gov/resource/f6w7-q2d2.json"
+const url = "https://data.wa.gov/resource/f6w7-q2d2.json";
 
 function init() {
-    let dropdownMenu = d3.select("#selDataset");
-    d3.json(url).then((data) => {
-        console.log(`Data: ${data}`);
-        let countyID = data.county;
-        countyID.forEach(county) => {
-            dropdownMenu.append("option").text(county);
-        });
-        let countyData = countyID[0];
-        bar(countyData);
+  let dropdownMenu = d3.select("#selDataset");
+  d3.json(url).then((data) => {
+    console.log(`Data: ${data}`);
+    let countyID = data.county;
+    countyID.forEach((county) => {
+      dropdownMenu.append("option").text(county);
+    });
+    let countyData = countyID[0];
+    bar(countyData);
+  });
 }
 
-// Part 2) - Make the bar chart
-//CODE HERE
-        let barData = [barTrace];
-        Plotly.newPlot('bar', barData);
+function bar(countyData) {
+  // Fetch the data for the selected countyData
+  // CODE HERE
 
-// Toggle the page to the new plots when the option has been changed using the dropdown Menu for Test Subject ID.
+  // Prepare the data for the bar chart
+  // CODE HERE
+  let barTrace = {
+    x: /* array of x-axis values */,
+    y: /* array of y-axis values */,
+    type: "bar"
+  };
+
+  let barData = [barTrace];
+
+  // Create the bar chart
+  Plotly.newPlot("bar", barData);
+}
+
+// Toggle the page to the new plots when the option has been changed using the dropdown menu for Test Subject ID.
 function optionChanged(selectValue) {
-    bar(selectValue);
+  bar(selectValue);
 }
 
 init();
