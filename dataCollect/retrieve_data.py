0b4d575779cf7f263113e3a1bab961386f14d7c2
Hanwen Jia <jiahw1115@gmail.com>
2023-05-22 18:49:06 +0100

    Update retrieve_data.py

--- dataCollect/retrieve_data.py
+++ dataCollect/retrieve_data.py
@@ -3,19 +3,29 @@ import pandas as pd
 import json
 import urllib.request
 # url define
-url1 = 'https://data.wa.gov/resource/3d5d-sdqb.csv'
+url1 = 'https://data.wa.gov/resource/3d5d-sdqb.json'
 url2 = 'https://data.wa.gov/resource/f6w7-q2d2.geojson'
 url3 = 'https://data.wa.gov/resource/f6w7-q2d2.json'
 
 # fist datasetoutput
-df = pd.read_csv(url1, delimiter=',')
+# Load json from url
+with urllib.request.urlopen(url1) as url:
+    data = json.loads(url.read().decode())
+# Convert list of dictionaries to DataFrame
+df = pd.DataFrame(data)
+# Export CSV
 df.to_csv('dataCollect/Size_History_By_County.csv', header = True, index = False)
-df.to_json('dataCollect/Size_History_By_County.json')
+# Convert DataFrame to JSON
+json_data = df.to_json(orient="records")
+# Export JSON
+with open('dataCollect/Size_History_By_County.json', 'w') as f:
+    f.write(json_data)
 
 # second dataset output
 response = requests.get(url2)
 with open('dataCollect/Population_Data.geojson', 'wb') as file:
     file.write(response.content)
+
 # third dataset output
 # Load json from url
 with urllib.request.urlopen(url3) as url:
