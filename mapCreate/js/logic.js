059b4bab05d2137d1959bc8108f2c4353a45f578
Hanwen Jia <jiahw1115@gmail.com>
2023-05-22 19:36:01 +0100

    Update logic.js

--- mapCreate/js/logic.js
+++ mapCreate/js/logic.js
@@ -14,6 +14,24 @@ var queryUrl = 'https://data.wa.gov/resource/f6w7-q2d2.geojson';
 
 var elecVehicleData;
 var elecVehicle;
+var colorMappings = {
+  'TESLA': "#ff0000",       // RED
+  'HONDA': "#00ff00",       // GREEN
+  'NISSAN': "#0000ff",      // BLUE
+  'VOLKSWAGEN': "#ff8000",  // ORANGE
+  'AUDI': "#ff00aa",        // PINK
+  'FORD': "#5500ff",        // PURPLE
+  'KIA': "#ffff00",         // YELLOW
+  'BMW': "#f5f5f5",         // WHITE
+  'CHEVROLET': "#ffff00",   // YELLOW
+  'JEEP': "#470024",        // DEEP RED
+  'CHRYSLER': "#fff4f9",    // Very pale (mostly white) pink
+  'TOYOTA': "#00ffff",      // Pure (or mostly pure) cyan.
+  'PORSCHE': "#274e13",     // DEEP GREEN
+  'VOLVO': "#0f3c64",       // DEEP BLUE
+  'POLESTAR': "#cc9a00",    // Strong yellow
+  'HYUNDAI': "#ccd000",     // Random
+};
 
 d3.json(queryUrl).then(function(data) {
   elecVehicleData = data.features;
@@ -27,48 +45,14 @@ function createFeatures(elecVehicleData) {
 
       var colorIndex = feature.properties.make;
 
-      var color;
-      if (colorIndex === 'TESLA') {
-        color = "#ff0000"; // RED
-      } else if (colorIndex === 'HONDA') {
-        color = "#00ff00"; // GREEN
-      } else if (colorIndex === 'NISSAN') {
-        color = "#0000ff"; // BLUE
-      } else if (colorIndex === 'VOLKSWAGEN') {
-        color = "#ff8000"; // ORANGE
-      } else if (colorIndex === 'AUDI') {
-        color = "#ff00aa"; // PINK
-      } else if (colorIndex === 'FORD') {
-        color = "#5500ff"; // PURPLE
-      } else if (colorIndex === 'KIA') {
-        color = "#ffff00"; // YELLOW
-      } else if (colorIndex === 'BMW') {
-        color = "#f5f5f5"; // white
-      } else if (colorIndex === 'CHEVROLET') {
-        color = "#ffff00"; // YELLOW
-      } else if (colorIndex === 'JEEP') {
-        color = "#470024"; // DEEP RED
-      } else if (colorIndex === 'CHRYSLER') {
-        color = "#fff4f9"; // Very pale (mostly white) pink
-      } else if (colorIndex === 'TOYOTA') {
-        color = "#00ffff"; // Pure (or mostly pure) cyan.
-      } else if (colorIndex === 'PORSCHE') {
-        color = "#274e13"; // DEEP GREEN
-      } else if (colorIndex === 'VOLVO') {
-        color = "#0f3c64"; // DEEP BLUE
-      } else if (colorIndex === 'POLESTAR') {
-        color = "#cc9a00"; // Strong yellow
-      } else if (colorIndex === 'HYUNDAI') {
-        color = "#ccd000"; // random
-      } else {
-        color = "#wcd123"; // random
-      }
-      
+      var color = colorMappings[colorIndex] || "#wcd123"; // default
+
       return L.circleMarker(latlng, {
         color: color,
         weight: 0.3,
+        radius : 6,
         opacity: 0.5,
-        fillOpacity: 1
+        fillOpacity: 0.5
       });
     },
     onEachFeature: function (feature, layer) {
@@ -92,58 +76,45 @@ function updateMap() {
   });
 
   // create new map
+  if (filteredFeatures.length > 0) {
   elecVehicle = L.geoJSON(filteredFeatures, {
     pointToLayer: function (feature, latlng) {
       var colorIndex = feature.properties.make;
 
-      var color;
-      if (colorIndex === 'TESLA') {
-        color = "#ff0000"; // RED
-      } else if (colorIndex === 'HONDA') {
-        color = "#00ff00"; // GREEN
-      } else if (colorIndex === 'NISSAN') {
-        color = "#0000ff"; // BLUE
-      } else if (colorIndex === 'VOLKSWAGEN') {
-        color = "#ff8000"; // ORANGE
-      } else if (colorIndex === 'AUDI') {
-        color = "#ff00aa"; // PINK
-      } else if (colorIndex === 'FORD') {
-        color = "#5500ff"; // PURPLE
-      } else if (colorIndex === 'KIA') {
-        color = "#ffff00"; // YELLOW
-      } else if (colorIndex === 'BMW') {
-        color = "#f5f5f5"; // white
-      } else if (colorIndex === 'CHEVROLET') {
-        color = "#ffff00"; // YELLOW
-      } else if (colorIndex === 'JEEP') {
-        color = "#470024"; // DEEP RED
-      } else if (colorIndex === 'CHRYSLER') {
-        color = "#fff4f9"; // Very pale (mostly white) pink
-      } else if (colorIndex === 'TOYOTA') {
-        color = "#00ffff"; // Pure (or mostly pure) cyan.
-      } else if (colorIndex === 'PORSCHE') {
-        color = "#274e13"; // DEEP GREEN
-      } else if (colorIndex === 'VOLVO') {
-        color = "#0f3c64"; // DEEP BLUE
-      } else if (colorIndex === 'POLESTAR') {
-        color = "#cc9a00"; // Strong yellow
-      } else if (colorIndex === 'HYUNDAI') {
-        color = "#ccd000"; // random
-      } else {
-        color = "#wcd123"; // random
-      }
+      var color = colorMappings[colorIndex] || "#wcd123"; // default
 
       return L.circleMarker(latlng, {
         color: color,
-        size : 0.5,
         weight: 0.3,
+        radius : 6,
         opacity: 0.5,
-        fillOpacity: 1
+        fillOpacity: 0.5
       });
     },
     onEachFeature: function (feature, layer) {
       layer.bindPopup("<h3>" + "VEHICLE ID: " + feature.properties.dol_vehicle_id + "</h3><hr><p>" + "MODEL: " + feature.properties.make + " / " +feature.properties.model + "</p><hr><p>" + "Year: " + feature.properties.model_year + "</p>");
     }
   }).addTo(map);
-}
+ }// select back to all vehicle
+  else {
+    elecVehicle = L.geoJSON( elecVehicleData, {
+      pointToLayer: function (feature, latlng) {
 
+        var colorIndex = feature.properties.make;
+
+      var color = colorMappings[colorIndex] || "#wcd123"; // default
+        
+        return L.circleMarker(latlng, {
+          color: color,
+          weight: 0.3,
+          radius : 6,
+          opacity: 0.5,
+          fillOpacity: 0.5
+        });
+      },
+      onEachFeature: function (feature, layer) {
+        layer.bindPopup("<h3>" + "VEHICLE ID: " + feature.properties.dol_vehicle_id + "</h3><hr><p>" + "MODEL: " + feature.properties.make + " / " +feature.properties.model + "</p><hr><p>" + "Year: " + feature.properties.model_year + "</p>");
+      }
+    }).addTo(map);
+  }
+}
